4d3924dd919130f4926901bf825dbea87d079fec
Juan Gabriel Chaves <achavesjg@gmail.com>
2024-06-26 01:10:12 -0300

    utilizar express handlebars para mostrar listado de productos

--- src/routes/views.routes.js
+++ src/routes/views.routes.js
@@ -1,6 +1,7 @@
 /** @format */
 
 import { Router } from "express";
+import ProductManager from "../controllers/productManager.js";
 
 const router = Router();
 router.get("/realtimeproducts", async (req, res) => {
@@ -12,8 +13,15 @@ router.get("/realtimeproducts", async (req, res) => {
     }
 });
 
-router.get("/products", async (req, res) => {
-    res.render("home");
+const productManager = new ProductManager("./src/models/products.json");
+
+router.get("/", async (req, res) => {
+    try {
+        const products = await productManager.getProducts();
+        res.render("home", { products });
+    } catch (error) {
+        res.status(500).json("Error interno del servidor");
+    }
 });
 
 export default router;
