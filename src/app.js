2f6e24b2425e26ccc9fae68f61540449314882c4
Juan Gabriel Chaves <achavesjg@gmail.com>
2024-06-25 21:33:40 -0300

    instalacion de dependencia de express handlebars y de socket.io y conf en app.js

--- src/app.js
+++ src/app.js
@@ -1,14 +1,23 @@
 /** @format */
 
 import express from "express";
+import { engine } from "express-handlebars";
+import { Server } from "socket.io";
 import productsRouter from "./routes/products.routes.js";
 import cartsRouter from "./routes/carts.routes.js";
 
 const app = express();
 const PUERTO = 8080;
 
+// Midlewares:
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static("/src/public"));
+
+// Motores de Plantillas - Express-Handlebars
+app.engine("handlebars", engine());
+app.set("view engine", "handlebars");
+app.set("views", "/src/views");
 
 app.use("/api/products", productsRouter);
 app.use("/api/carts", cartsRouter);
